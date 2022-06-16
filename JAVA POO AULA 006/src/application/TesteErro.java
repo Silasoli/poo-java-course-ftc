7ff79acbef063995f45e3018e50d37742a8ab9e2
Silasoli <silasoliv39@gmail.com>
2022-06-16 19:30:40 -0300

    Add files via upload

---
+++ JAVA POO AULA 006/src/application/TesteErro.java
@@ -0,0 +1,41 @@
+package application;
+
+import model.Conta;
+
+/**
+ *
+ * @author silas
+ */
+public class TesteErro {
+
+    public static void main(String[] args) {
+        System.out.println("Inicio do main");
+        metodo1();
+        System.out.println("Fim do main");
+
+    }
+
+    static void metodo1() {
+        System.out.println("Inicio do metodo1");
+        metodo2();
+        System.out.println("Fim do metodo1");
+    }
+
+    static void metodo2() {
+        System.out.println("inicio do metodo2");
+        Conta cc = new Conta();
+        try {
+        for (int i = 0; i <= 15; i++) {
+            cc.deposita(i + 1000);
+            System.out.println(cc.getSaldo());
+            if (i == 5) {
+                cc = null;
+            }
+        }
+        System.out.println("fim do metodo2");
+    }
+        catch (NullPointerException e) {
+            System.out.println("erro" +e);
+        }
+    }
+}
